9d360e32d8e29f7274861dadc9f7a75e3d8a4d15
Darko Palic <darko.palic@xenovation.com>
2018-09-08 22:31:25 +0200

    final merge

--- src/jsp/assets/core/js/option_title.js
+++
@@ -1,7 +0,0 @@
-function addTitleToOptions() {
-    var options = document.getElementsByTagName('option');
-    for (var i = 0; i < options.length; i++) {
-        optionText = options[i].innerHTML;
-        options[i].title = optionText;
-    }
-}
